2f85b22cf2785a780ed5fcbef816d7f7ccb87305
zcy <1229059439@qq.com>
2020-12-13 15:47:43 +0800

    change name

--- socket/socket.h
+++ socket/socket.h
@@ -1,7 +1,7 @@
 /**
  * @copybrief
  * MIT License
- * Copyright (c) 2020 zcy & NeilKleistGao
+ * Copyright (c) 2020 chui & NeilKleistGao
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
